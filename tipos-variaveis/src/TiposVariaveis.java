dbe0075b8a317cfaf32ad31295e1198b5fe6dcb8
Marcelo Claro <marc01mcl@hotmail.com>
2025-02-14 09:26:48 -0300

    criar variáveis e constantes

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,29 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       
+        // Variáveis
+       double salario = 2500;
+       System.out.println(salario);
+
+       boolean verdadeiro = true;
+       boolean falso = false;
+       System.out.println(verdadeiro);
+       System.out.println(falso);
+
+       String nome = "Lucas";
+       System.out.println(nome);
+
+       short numeroCurto = 1;
+       int numeroNormal = numeroCurto;
+       short numeroCurto2 = (short) numeroNormal;
+       System.out.println(numeroCurto2);
+
+       int numero = 5;
+       numero = 10;
+       System.out.println(numero);
+
+       //    Constantes
+       final double VALOR_DE_PI = 3.14;
+       System.out.println(VALOR_DE_PI);
     }
 }
