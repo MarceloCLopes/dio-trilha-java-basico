da8efd4dc66e3efd15865e96cc2a63d2c00aefe8
Marcelo Claro <marc01mcl@hotmail.com>
2025-02-14 10:32:55 -0300

    criando operadores relacionais

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,11 +1,26 @@
 public class Operadores {
   public static void main(String[] args) {
-    int a, b;
-    a = 10;
-    b = 10;
+    int numero1, numero2;
+    numero1 = 1;
+    numero2 = 2;
     
     // Operador ternário
-    String resultado = a == b ? "verdadeiro" : "falso";
+    String resultado = numero1 == numero2 ? "verdadeiro" : "falso";
     System.out.println(resultado);
+    
+    // Operadores relacionais
+    boolean simNao = numero1 == numero2;
+    
+    if (numero1 < numero2) {
+      System.out.println("a nossa condição é verdadeira");
+    }
+
+    System.out.println("numero1 é igual a numero2? " + simNao);
+   
+    simNao = numero1 != numero2;
+    System.out.println("numero1 é diferente a numero2? " + simNao);
+    
+    simNao = numero1 > numero2;
+    System.out.println("numero1 é maior que numero2? " + simNao); 
   }
 }
