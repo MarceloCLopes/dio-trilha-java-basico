be6cd7045bacb3d107e5a46ff11d3d37e62c501a
Marcelo Claro <marc01mcl@hotmail.com>
2025-02-14 10:17:41 -0300

    criando operador ternário

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,11 @@
+public class Operadores {
+  public static void main(String[] args) {
+    int a, b;
+    a = 10;
+    b = 10;
+    
+    // Operador ternário
+    String resultado = a == b ? "verdadeiro" : "falso";
+    System.out.println(resultado);
+  }
+}
